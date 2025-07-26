dc1889c52f7d94cbfd0a1dd97bc22db51cdc61b8
Sojwal Magar <sojwalco09@gmail.com>
2025-07-27 00:30:41 +0530

    resto table list

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app = express();
 const cors =require("cors");
+const {jwtAuth}=require("./utils/jwtauth")
 const userRouter=require("./Routers/Users")
 const restoRouter=require("./Routers/Resto")
 const tableRouter=require("./Routers/Table")
@@ -9,6 +10,7 @@ const adminRouter=require("./Routers/Admin")
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(jwtAuth);
 app.use("/user",userRouter);
 app.use("/resto",restoRouter);
 app.use("/table",tableRouter);
