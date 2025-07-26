392868737a47bce49c1258528b329914d5028b5e
Sojwal Magar <sojwalco09@gmail.com>
2025-07-26 19:58:59 +0530

    data update

--- Resto_Database.sql
+++ Resto_Database.sql
@@ -15,14 +15,28 @@ CREATE TABLE users (
     
 );
 
+insert into users(user_id,name,email,password,phone,role)values
+(1,'Aditya Kamble','adityakamble@gmail.com',1234,'a@123','Owner'),
+(2,'Sojwal Magar','sojwalmagar@gmail.com',5678,'s@123','Owner'),
+(3,'Madhura Vyavahare','madhura@gmail.com',6789,'m@123','Owner'),
+(4,'Nishigandha Kolapkar','nishi@gmail.com',7896,'n@123','Owner');
+
 -- 2. Restaurants
 CREATE TABLE restaurants (
     resto_id INT PRIMARY KEY AUTO_INCREMENT,
     owner_id INT,
     name VARCHAR(100),
     document TEXT,
-    location VARCHAR(100)
+    location VARCHAR(100),
+    is_blocked TINYINT(1) DEFAULT 1
 );
+
+insert into restaurants(resto_id,owner_id,name,document,location)values
+(1,01,'Cafe Bliss','Aadhaar card','Nashik'),
+(2,02,'Cafe Goodluck','Aadhaar card','Pune'),
+(3,03,'Hotel Sai Sagar','Aadhaar card','Pune'),
+(4,04,'Cafe Marigold','Aadhaar card','Mumbai');
+
 
 -- 3. Menu Items
 CREATE TABLE menu_items (
@@ -33,6 +47,13 @@ CREATE TABLE menu_items (
     category VARCHAR(50)
 );
 
+insert into menu_items(item_id,resto_id,item_name,price,category)values
+(101,1,'Veg Sandwich',70,'Snacks'),
+(102,2,'Dosa',90,'Snacks'),
+(103,3,'Rabdi',70,'Dessert'),
+(104,4,'Ice-Cream',80,'Dessert'),
+(105,5,'Noodles',70,'Snacks');
+
 -- 4. Restaurant Tables
 CREATE TABLE restaurant_tables (
     table_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -42,6 +63,11 @@ CREATE TABLE restaurant_tables (
     category enum('Gold', 'Silver', 'Premium')
 );
 
+insert into restaurant_tables(table_id,resto_id,capacity,charge,category)values
+(1001,1,4,635.8,'Gold'),
+(1002,2,6,567.8,'Silver'),
+(1003,3,2,978.8,'Premium');
+
 -- 5. Reviews
 CREATE TABLE reviews (
     review_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -50,6 +76,12 @@ CREATE TABLE reviews (
     comment TEXT,
     rating INT CHECK (rating BETWEEN 1 AND 5)
 );
+
+insert into reviews(review_id,resto_id,customer_id,comment,rating)values
+(123,1,001,'Good Ambiance',4),
+(125,2,002,'Affordable',3),
+(153,1,003,'Good Ambiance',3);
+
 
 -- 6. Bookings
 CREATE TABLE bookings (
@@ -61,6 +93,11 @@ CREATE TABLE bookings (
     end_time DATETIME,
     status enum('Booked', 'Cancelled', 'Completed')
 );
+
+
+
+
+
 -- 7. Orders
 CREATE TABLE orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -69,6 +106,9 @@ CREATE TABLE orders (
     status enum ('Pending', 'Confirmed', 'Delivered')
 );
 
+
+
+
 -- 8. Order Details
 CREATE TABLE order_details (
     detail_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -76,6 +116,7 @@ CREATE TABLE order_details (
     item_id INT,
     quantity INT
 );
+
 
 -- 9. Bills
 CREATE TABLE bills (
